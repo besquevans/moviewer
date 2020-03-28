e1feca9a9b59f68ef83afb9449eb179503527af5
Jimmy <du20783@gmail.com>
2020-03-28 17:02:41 +0800

    add Header vue

---
+++ app/javascript/src/components/Header.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-toolbar flat color="white">
+    <v-toolbar-title>
+      <router-link to="/">{{ title }}</router-link>
+    </v-toolbar-title>
+    <v-spacer></v-spacer>
+
+    <router-link to="omdb">
+      <v-btn>{{ search }}</v-btn>
+    </router-link>
+
+    <div class="user-unlogin-container" v-if="!isLogin">
+      <v-btn @click="handleSignUp">Sign up</v-btn>
+      <v-btn @click="handleLogin">Login</v-btn>
+    </div>
+
+    <div class="user-login-container" v-else>
+      <p class="user-email">{{ userEmail }}</p>
+      <v-btn @click="handleLogout">Log out</v-btn>
+    </div>
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "Moviewer",
+      search: "Search",
+      isLogin: false,
+      userEmail: "test@mail.com"
+    };
+  },
+  methods: {
+    handleSignUp: function() {
+      this.$router.push("/signup");
+    },
+    handleLogin: function() {
+      this.$router.push("/login");
+    },
+    handleLogout: function() {
+      console.log("logout");
+    }
+  }
+};
+</script>
--- app/javascript/src/components/film.vue
+++ app/javascript/src/components/film.vue
@@ -1,48 +1,40 @@
 <template>
   <v-data-table :headers="headers" :items="films" sort-by="id" class="elevation-1">
     <template v-slot:top>
-      <v-toolbar flat color="white">
-        <v-toolbar-title>Films</v-toolbar-title>
-        <v-divider class="mx-4" inset vertical></v-divider>
-        <v-spacer></v-spacer>
+      <v-dialog v-model="dialog" max-width="800px">
+        <template v-slot:activator="{ on }">
+          <v-btn color="primary" dark class="mb-2" v-on="on">New Film</v-btn>
+        </template>
 
-        <router-link to="omdb">Search</router-link>
+        <!--------edit or update film------------->
+        <v-card>
+          <v-card-title>
+            <span class="headline">{{ formTitle }}</span>
+          </v-card-title>
 
-        <v-dialog v-model="dialog" max-width="800px">
-          <template v-slot:activator="{ on }">
-            <v-btn color="primary" dark class="mb-2" v-on="on">New Film</v-btn>
-          </template>
+          <v-card-text>
+            <v-container>
+              <v-row>
+                <v-col cols="12" sm="6" md="4">
+                  <v-text-field v-model="editedItem.title" label="Title"></v-text-field>
+                </v-col>
+                <v-col cols="12" sm="6" md="4">
+                  <v-text-field v-model="editedItem.year" label="Year"></v-text-field>
+                </v-col>
+                <v-col cols="12" sm="6" md="4">
+                  <v-text-field v-model="editedItem.intro" label="Intro"></v-text-field>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-card-text>
 
-          <!--------edit or update film------------->
-          <v-card>
-            <v-card-title>
-              <span class="headline">{{ formTitle }}</span>
-            </v-card-title>
-
-            <v-card-text>
-              <v-container>
-                <v-row>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field v-model="editedItem.title" label="Title"></v-text-field>
-                  </v-col>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field v-model="editedItem.year" label="Year"></v-text-field>
-                  </v-col>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field v-model="editedItem.intro" label="Intro"></v-text-field>
-                  </v-col>
-                </v-row>
-              </v-container>
-            </v-card-text>
-
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn color="blue darken-1" text @click="close">Cancel</v-btn>
-              <v-btn color="blue darken-1" text @click="save(editedItem)">Save</v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
-      </v-toolbar>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="blue darken-1" text @click="close">Cancel</v-btn>
+            <v-btn color="blue darken-1" text @click="save(editedItem)">Save</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
     </template>
 
     <template v-slot:item.action="{ item }">
