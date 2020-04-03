1df2b42ba33f75d2a0c32ecef4bb057a07e81dcf
Jimmy <du20783@gmail.com>
2020-04-03 13:08:31 +0800

    add api controller

---
+++ app/controllers/api/v1/films_controller.rb
@@ -0,0 +1,44 @@
+class Api::V1::FilmsController < ApiController
+  def index
+		@films = Film.all
+		render json: @films 
+	end
+
+	def show 
+		@film = Film.find(params[:id])
+		render json: @film
+	end
+
+	def create
+		@film = Film.new(film_params)
+		if @film.save
+			render json: { status: :ok, message: 'Create Success' }
+		else
+			render json: { json: @film.errors, status: :unprocessable_entity }
+		end
+	end
+
+	def update
+		@film = Film.find(params[:id])
+		if @film.update(film_params)
+			render json: { status: :ok, message: 'Update Success' }
+		else
+			render json: { json: @film.errors, status: :unprocessable_entity }
+		end
+	end
+
+	def destroy
+		@film = Film.find(params[:id])
+		if @film.destroy
+			render json: { json: 'The film was successfully deleted.'}
+		else
+			render json: { json: @film.errors, status: :unprocessable_entity }
+		end
+	end
+
+	private
+
+	def film_params
+		params.require(:film).permit(:title, :year, :intro)
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   get 'welcome/index'
 
   resources :films
+  namespace :api, defaults: {format: :json} do
+    namespace :v1 do
+      resources :films, only: [:index, :show, :create, :update, :desdtroy]
+    end
+  end
+
   get 'app', to: 'films#index'
 
   root to: 'welcome#index'
