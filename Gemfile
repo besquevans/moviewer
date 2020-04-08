26d30b3e524d1ab08f22f80f44cb9af76d4a8c9f
Jimmy <du20783@gmail.com>
2020-04-08 23:05:32 +0800

    add gem devise

--- Gemfile
+++ Gemfile
@@ -29,6 +29,7 @@ gem 'jbuilder', '~> 2.7'
 gem 'bootsnap', '>= 1.4.2', require: false
 #user secert password
 gem 'bcrypt'
+gem 'devise'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'welcome/index'
 
   resources :films
   namespace :api do
     namespace :v1 do
       resources :films, only: [:index, :show, :create, :update, :destroy]
+
+      post 'signup', to: 'users#create', as: 'signup'
+      post 'login', to: 'sessions#create', as: 'login'
+      post 'logout', to: 'sessions#destroy', as: 'logout'
     end
   end
 
